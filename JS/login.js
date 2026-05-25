9eaccacb6d241b29817786a0813143ded7a1266e
gustavo01dd <gugaasilva2008@gmail.com>
2026-05-24 22:38:23 -0300

    alteração senha

--- JS/login.js
+++ JS/login.js
@@ -4,7 +4,7 @@ function login(event){
     const rm = document.getElementById('rm').value;
     const senha = document.getElementById('password').value;
 
-    if(rm === "26587" && senha === "Astro2024@"){
+    if(rm === "26587" && senha === "Etec2024@"){
         window.location.href = "home.html";
     }else{
         alert("RM ou senha incorretos");
